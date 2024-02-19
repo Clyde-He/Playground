10d441cdd811364ed696d3584f8ddae4d068bc61
Clyde He <ClydeStevensHenry@outlook.com>
2024-02-18 22:30:13 -0800

    Initial Commit

---
+++ Playground/PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundApp.swift
+//  Playground
+//
+//  Created by Clyde He on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
