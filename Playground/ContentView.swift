e771a58936ccc8132f54bacb6f971f88b3537213
Clyde He <clydestevenshenry@outlook.com>
2024-02-20 02:09:21 -0800

    Added Fidget Card.swift

--- Playground/ContentView.swift
+++ Playground/ContentView.swift
@@ -11,8 +11,9 @@ struct ContentView: View {
 	
 	var body: some View {
 		TabView {
+            
+            // Profile Card
 			ZStack (alignment: .topLeading){
-				
 				VStack (alignment: .leading, spacing: 4) {
 					Text("Feb 19th, 2024")
 						.font(.system(size: 16, weight: .medium, design: .rounded))
@@ -27,8 +28,9 @@ struct ContentView: View {
 					.frame(maxWidth: .infinity, maxHeight: .infinity)
 				
 			}
-			ZStack (alignment: .topLeading){
-				
+            
+            // Fidget Card
+			VStack (alignment: .leading){
 				VStack (alignment: .leading, spacing: 4) {
 					Text("Feb 20th, 2024")
 						.font(.system(size: 16, weight: .medium, design: .rounded))
@@ -36,13 +38,14 @@ struct ContentView: View {
 					Text("Fidget Card")
 						.font(.system(size: 28, weight: .bold, design: .rounded))
 				}
-				.padding(24)
+				.padding(EdgeInsets(top: 24, leading: 24, bottom: 0, trailing: 24))
 				.background(Color.clear.ignoresSafeArea(edges: []))
 				
 				FidgetCard()
 					.frame(maxWidth: .infinity, maxHeight: .infinity)
-				
+                    .padding(.bottom, 48)
 			}
+            
 		}
 		.tabViewStyle(PageTabViewStyle())
 	}
--- Playground/Fidget Card.swift
+++ Playground/Fidget Card.swift
@@ -8,44 +8,66 @@
 import SwiftUI
 
 struct FidgetCard: View {
-	
-	@State var cardWidth : CGFloat = 320
-	@State var cardHeight : CGFloat = 240
-//	@State var rotationDegree : CGSize = (0,0)
-	
+    
+    @State var cardWidth : CGFloat = 320
+    @State var cardHeight : CGFloat = 240
+    @State var dragLocation = CGPoint(x: 0, y: 0)
+    var intensity : CGFloat = 10
+    
+    func linearScale(inMin: CGFloat, inMax: CGFloat, outMin: CGFloat, outMax: CGFloat, inValue: CGFloat) -> CGFloat {
+        return inValue * (outMax - outMin) / (inMax - inMin) + outMin
+    }
+    
     var body: some View {
-		ZStack {
-			
-			//Background
-			Color.red
-				.ignoresSafeArea()
-			
-			VStack (spacing: 128) {
-				
-				Text("Some value")
-				
-				ZStack {
-					
-						//Colored Rectangle
-					RoundedRectangle(cornerRadius: 24, style: .continuous)
-						.fill(Color.blue)
-						.frame(width: cardWidth, height: cardHeight)
-					
-						//Drag Card
-					RoundedRectangle(cornerRadius: 20, style: .continuous)
-						.fill(Color.black)
-						.frame(width: cardWidth - 8, height: cardHeight - 8)
-						.rotation3DEffect(
-							.degrees(50),axis: (x: 0.0, y: 1.0, z: 0.0),perspective: 1.0
-						)
-						.gesture(TapGesture(count: 1).onEnded({
-							cardWidth += 10
-						}))
-					
-				}
-				
-			}
-		}
+        ZStack {
+            
+            VStack (spacing: 48) {
+                
+                ZStack {
+                    
+                    //Colored Rectangle
+                    RoundedRectangle(cornerRadius: 24, style: .continuous)
+                        .fill(Color.blue)
+                        .frame(width: cardWidth, height: cardHeight)
+                    
+                    //Drag Card
+                    RoundedRectangle(cornerRadius: 20, style: .continuous)
+                        .fill(Color(UIColor.systemBackground))
+                        .frame(width: cardWidth - 8, height: cardHeight - 8)
+                        .rotation3DEffect(
+                            .degrees(dragLocation.x),axis: (x: 0.0, y: 1.0, z: 0.0),perspective: 1.0
+                        )
+                        .rotation3DEffect(
+                            .degrees(-dragLocation.y),axis: (x: 1, y: 0.0, z: 0.0),perspective: 1.0
+                        )
+                        .gesture(
+                            DragGesture(minimumDistance: 0)
+                                .onChanged() { value in
+                                    withAnimation(.interactiveSpring) {
+                                        dragLocation = CGPoint(x: linearScale(inMin: 0, inMax: cardWidth, outMin: -intensity, outMax: intensity, inValue: value.location.x), y: linearScale(inMin: 0, inMax: cardHeight, outMin: -intensity, outMax: intensity, inValue: value.location.y))
+                                    }
+                                }
+                                .onEnded() { _ in
+                                    withAnimation(.spring(duration: 0.3, bounce: 0.7), {
+                                        dragLocation = .zero
+                                    })
+                                }
+                            
+                        )
+                    
+                }
+                HStack {
+                    Text(String(format: "%.2f",dragLocation.x) + "," + String(format: "%.2f",dragLocation.y))
+                        .font(.system(size: 16, weight: .medium, design: .monospaced))
+                        .foregroundStyle(Color.primary)
+                }
+                .padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20))
+                .background(RoundedRectangle(cornerRadius: 48, style: .continuous)
+                    .fill(Color.primary.opacity(0.03))
+                    .strokeBorder(Color.primary.opacity(0.05), style: StrokeStyle(lineWidth: 1)))
+                .animation(.easeInOut(duration: 0.15), value: dragLocation)
+            }
+        }
     }
 }
 
