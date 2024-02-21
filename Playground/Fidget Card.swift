8b4f6cc8af8898bf069094b0adb58b925fea653c
Clyde He <clydestevenshenry@outlook.com>
2024-02-21 01:12:08 -0800

    Update Fidget Card.swift

--- Playground/Fidget Card.swift
+++ Playground/Fidget Card.swift
@@ -11,11 +11,11 @@ struct FidgetCard: View {
     
     @State var cardWidth : CGFloat = 320
     @State var cardHeight : CGFloat = 240
-    @State var dragLocation = CGPoint(x: 0, y: 0)
-    var intensity : CGFloat = 10
+    @State var dragDegree = CGPoint(x: 0, y: 0)
+    var intensity : CGFloat = 20
     
     func linearScale(inMin: CGFloat, inMax: CGFloat, outMin: CGFloat, outMax: CGFloat, inValue: CGFloat) -> CGFloat {
-        return inValue * (outMax - outMin) / (inMax - inMin) + outMin
+        return (inValue - inMin) * (outMax - outMin) / (inMax - inMin) + outMin
     }
     
     var body: some View {
@@ -35,29 +35,59 @@ struct FidgetCard: View {
                         .fill(Color(UIColor.systemBackground))
                         .frame(width: cardWidth - 8, height: cardHeight - 8)
                         .rotation3DEffect(
-                            .degrees(dragLocation.x),axis: (x: 0.0, y: 1.0, z: 0.0),perspective: 1.0
+                            .degrees(dragDegree.x),axis: (x: 0.0, y: 1.0, z: 0.0),perspective: 1.0
                         )
                         .rotation3DEffect(
-                            .degrees(-dragLocation.y),axis: (x: 1, y: 0.0, z: 0.0),perspective: 1.0
+                            .degrees(-dragDegree.y),axis: (x: 1, y: 0.0, z: 0.0),perspective: 1.0
                         )
                         .gesture(
                             DragGesture(minimumDistance: 0)
                                 .onChanged() { value in
                                     withAnimation(.interactiveSpring) {
-                                        dragLocation = CGPoint(x: linearScale(inMin: 0, inMax: cardWidth, outMin: -intensity, outMax: intensity, inValue: value.location.x), y: linearScale(inMin: 0, inMax: cardHeight, outMin: -intensity, outMax: intensity, inValue: value.location.y))
+                                        let normalizedX = linearScale(inMin: 0-cardWidth*0.2, inMax: cardWidth+cardWidth*0.2, outMin: -1, outMax: 1, inValue: value.location.x)
+                                        let normalizedY = linearScale(inMin: 0-cardHeight, inMax: cardHeight+cardHeight, outMin: -1, outMax: 1, inValue: value.location.y)
+                                        let dragDegreeX : CGFloat = sqrt(min(abs(normalizedX),1)) * intensity * normalizedX / abs(normalizedX)
+                                        let dragDegreeY : CGFloat = sqrt(min(abs(normalizedY),1)) * intensity * normalizedY / abs(normalizedY)
+                                        
+                                        dragDegree = CGPoint(x: dragDegreeX, y: dragDegreeY)
                                     }
                                 }
                                 .onEnded() { _ in
                                     withAnimation(.spring(duration: 0.3, bounce: 0.7), {
-                                        dragLocation = .zero
+                                        dragDegree = .zero
                                     })
                                 }
                             
                         )
                     
+                    //Parallax Overlay
+                    VStack(alignment: .leading, spacing: 2) {
+                        Text("Clyde He")
+                            .font(.system(size: 20, weight: .semibold, design: .serif))
+                        Text("@clyde_he")
+                            .font(.system(size: 12, weight: .regular, design: .monospaced))
+                            .opacity(0.4)
+                    }
+                    .padding(24)
+                    .frame(width: cardWidth, height: cardHeight, alignment: .bottomLeading)
+                    .rotation3DEffect(
+                        .degrees(dragDegree.x),
+                        axis: (x: 0.0, y: 1.0, z: 0.0),
+                        anchor: .center,
+                        anchorZ: 0.0,
+                        perspective: 0.5
+                    )
+                    .rotation3DEffect(
+                        .degrees(-dragDegree.y),
+                        axis: (x: 1.0, y: 0.0, z: 0.0),
+                        anchor: .center,
+                        anchorZ: 0.0,
+                        perspective: 0.5
+                    )
+                    
                 }
                 HStack {
-                    Text(String(format: "%.2f",dragLocation.x) + "," + String(format: "%.2f",dragLocation.y))
+                    Text(String(format: "%.2f",dragDegree.x) + "," + String(format: "%.2f",dragDegree.y))
                         .font(.system(size: 16, weight: .medium, design: .monospaced))
                         .foregroundStyle(Color.primary)
                 }
@@ -65,7 +95,7 @@ struct FidgetCard: View {
                 .background(RoundedRectangle(cornerRadius: 48, style: .continuous)
                     .fill(Color.primary.opacity(0.03))
                     .strokeBorder(Color.primary.opacity(0.05), style: StrokeStyle(lineWidth: 1)))
-                .animation(.easeInOut(duration: 0.15), value: dragLocation)
+                .animation(.easeInOut(duration: 0.15), value: dragDegree)
             }
         }
     }
