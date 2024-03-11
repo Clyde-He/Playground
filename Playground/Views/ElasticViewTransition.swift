92f7bf4fe9986f08e4ff308fbd8e7b9b0c437995
Clyde He <ClydeStevensHenry@outlook.com>
2024-03-10 17:30:52 -0700

    Finalized Elastic View Transition Demo

--- Playground/Views/ElasticViewTransition.swift
+++ Playground/Views/ElasticViewTransition.swift
@@ -17,12 +17,16 @@ struct ElasticViewTransition: View {
 	@State private var scrollElasticOffset = 0.0
 	@State private var profileActualSize = 48.0
 	@State private var profileOffset = 0.0
+	@State private var foregroundOpacity = 1.0
 	@State private var viewTransitioned = false
 	
-	@State private var showNavBar = true
+	@State private var hideControls = false
 	
 	let impact = UIImpactFeedbackGenerator(style: .medium)
 	@State private var impactOccurred = false
+	
+	@Environment(\.colorScheme) var colorScheme
+	@State private var isDarkModeEnabled = false
 	
 	func linearNormalization(inMin: CGFloat, inMax: CGFloat, outMin: CGFloat, outMax: CGFloat, inValue: CGFloat) -> CGFloat {
 		return (inValue - inMin) * (outMax - outMin) / (inMax - inMin) + outMin
@@ -39,17 +43,17 @@ struct ElasticViewTransition: View {
 					ScrollView(.horizontal, showsIndicators: false) {
 						HStack(spacing: 0) {
 							
-							RoundedRectangle(cornerRadius: 0, style: .continuous)
-								.foregroundColor(.red)
-								.aspectRatio(3 / 2, contentMode: .fit)
+							Image("TestPhoto01")
+								.resizable()
+								.aspectRatio(contentMode: .fit)
 								.containerRelativeFrame(.horizontal)
-							RoundedRectangle(cornerRadius: 0, style: .continuous)
-								.foregroundColor(.green)
-								.aspectRatio(1 / 1, contentMode: .fit)
+							Image("TestPhoto02")
+								.resizable()
+								.aspectRatio(contentMode: .fit)
 								.containerRelativeFrame(.horizontal)
-							RoundedRectangle(cornerRadius: 0, style: .continuous)
-								.foregroundColor(.blue)
-								.aspectRatio(3 / 4, contentMode: .fit)
+							Image("TestPhoto03")
+								.resizable()
+								.aspectRatio(contentMode: .fit)
 								.containerRelativeFrame(.horizontal)
 							GeometryReader { geometry in
 								Color.clear
@@ -67,9 +71,10 @@ struct ElasticViewTransition: View {
 											profileActualSize = linearNormalization(inMin: 0, inMax: transitionThreshold, outMin: 48, outMax: 66, inValue: scrollElasticOffset)
 											
 											// Calculate ProfileOffset
-											
-											
 											profileOffset = linearNormalization(inMin: 0, inMax: transitionThreshold, outMin: 0, outMax: (transitionThreshold - profileActualSize) / 2, inValue: scrollElasticOffset)
+											
+											// Calculate ForegroundOpacity
+											foregroundOpacity = linearNormalization(inMin: 0, inMax: transitionThreshold, outMin: 1, outMax: 0.5, inValue: scrollElasticOffset)
 											
 										}
 										
@@ -91,9 +96,6 @@ struct ElasticViewTransition: View {
 						.frame(maxHeight: UIScreen.main.bounds.width / 3 * 4)
 					}
 					.scrollTargetBehavior(.paging)
-					.onAppear() {
-						impact.prepare()
-					}
 					
 					// Controls
 					
@@ -102,27 +104,12 @@ struct ElasticViewTransition: View {
 					VStack(spacing: 16) {
 						Text(String(format: "%.2f, %.2f, %.2f", scrollElasticOffset, profileActualSize, profileOffset))
 							.font(.system(size: 16, weight: .medium, design: .monospaced))
-							.foregroundStyle(.primary.opacity(0.6))
+							.foregroundStyle(Color.primary)
 							.padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20))
-							.background(RoundedRectangle(cornerRadius: 8, style: .continuous)
-								.fill(Color.primary.opacity(0.03)))
-							.opacity(showNavBar ? 1 : 0)
-						
-						Button(action: {
-							withAnimation(.linear) {
-								showNavBar.toggle()
-							}
-						},
-							   label: {
-							Text("Hide Controls")
-								.font(.system(size: 16, weight: .medium, design: .monospaced))
-								.foregroundStyle(Color.primary)
-								.padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20))
-								.background(RoundedRectangle(cornerRadius: 48, style: .continuous)
-									.fill(Color.primary.opacity(0.03))
-									.strokeBorder(Color.primary.opacity(0.05), style: StrokeStyle(lineWidth: 1)))
-								.opacity(showNavBar ? 1 : 0)
-						})
+							.background(RoundedRectangle(cornerRadius: 48, style: .continuous)
+								.fill(Color.primary.opacity(0.03))
+								.strokeBorder(Color.primary.opacity(0.05), style: StrokeStyle(lineWidth: 1)))
+							.opacity(hideControls ? 0 : 1)
 					}
 					
 					Spacer()
@@ -131,26 +118,60 @@ struct ElasticViewTransition: View {
 				.padding(.top, geometry.safeAreaInsets.top)
 				.padding(.bottom, geometry.safeAreaInsets.bottom)
 				
+				
+				// Foreground
+				
+				if hideControls {
+					Image("Foreground")
+						.resizable()
+						.allowsHitTesting(false)
+						.opacity(foregroundOpacity)
+					
+				}
+				
 				// Profile View
 				
-				RoundedRectangle(cornerRadius: profileActualSize / 2, style: .continuous)
-					.fill(.gray)
-					.frame(width: viewTransitioned ? UIScreen.main.bounds.width : profileActualSize , height: viewTransitioned ? UIScreen.main.bounds.height : profileActualSize)
-					.offset(x: viewTransitioned ? 0 : -6 - profileOffset , y: viewTransitioned ? 0 : -391)
-					.gesture(
-						TapGesture()
-							.onEnded() {
-								viewTransitioned.toggle()
-								impact.impactOccurred()
-								impactOccurred.toggle()
-							}
-					)
-					.animation(.spring(duration: 0.3, bounce: 0.2), value: viewTransitioned)
+				ZStack {
+					Image("UserAvatar")
+						.resizable()
+						.aspectRatio(contentMode: .fit)
+						.opacity(viewTransitioned ? 0 : 1)
+					Image("Profile")
+						.resizable()
+						.aspectRatio(contentMode: .fill)
+						.opacity(viewTransitioned ? 1 : 0)
+				}
+				.background(.white)
+				.frame(width: viewTransitioned ? UIScreen.main.bounds.width : profileActualSize , height: viewTransitioned ? UIScreen.main.bounds.height : profileActualSize)
+				.clipShape(RoundedRectangle(cornerRadius: profileActualSize / 2, style: .continuous))
+				.offset(x: viewTransitioned ? 0 : -6 - profileOffset , y: viewTransitioned ? 0 : -424)
+				.gesture(
+					TapGesture()
+						.onEnded() {
+							viewTransitioned.toggle()
+							impact.impactOccurred()
+							impactOccurred.toggle()
+						}
+				)
+				.animation(.smooth(duration: 0.35), value: viewTransitioned)
 			}
+			.background(Color(UIColor.systemBackground))
 			.ignoresSafeArea()
+			.onTapGesture {
+				withAnimation(.smooth(duration: 0.3)) {
+					hideControls.toggle()
+				}
+			}
+		}
+		.onAppear() {
+			if colorScheme == .dark {
+				isDarkModeEnabled = true
+			}
+			impact.prepare()
 		}
 		.navigationBarTitleDisplayMode(.inline)
-		.toolbar(showNavBar ? .visible : .hidden, for: .navigationBar)
+		.toolbar(hideControls ? .hidden : .visible, for: .navigationBar)
+		.environment(\.colorScheme, hideControls ? .dark : (isDarkModeEnabled ? .dark : .light))
 	}
 }
 
